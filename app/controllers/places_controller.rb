f4c51f1f53e977b246fa5fa213702e5a43d5d8f0
Bergen Carloss <bergen.carloss@kellogg.northwestern.edu>
2024-03-10 20:40:37 +0000

    commit9

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,18 +2,16 @@ class PlacesController < ApplicationController
   before_action :require_login, except: [:index, :show]
 
   def index
-    if current_user
-      @places = current_user.places
-    else
-      redirect_to login_path, alert: 'You must be logged in to access this section'
-    end
+    @places = current_user ? current_user.places : []
   end
 
   def show
-    @place = current_user.places.find(params[:id])
-    @entries = @place.entries
-  rescue ActiveRecord::RecordNotFound
-    redirect_to places_path, alert: 'Place not found or not accessible.'
+    @place = current_user.places.find_by(id: params[:id])
+    if @place
+      @entries = @place.entries
+    else
+      redirect_to places_path, alert: 'Place not found or not accessible.'
+    end
   end
 
   def new
@@ -37,9 +35,10 @@ class PlacesController < ApplicationController
 
   def require_login
     unless current_user
-      flash[:alert] = "You must be logged in to access this section"
+      flash[:alert] = 'You must be logged in to access this section'
       redirect_to login_path
     end
   end
 end
+
 
