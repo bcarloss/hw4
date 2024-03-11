fa4231d98218b18015f88a1791f395a3783e046f
Bergen Carloss <bergen.carloss@kellogg.northwestern.edu>
2024-03-11 01:39:20 +0000

    commit13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :user_signed_in?
 
   private
 
-  # Sets @current_user with the user found by the session user_id
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  # Returns a boolean indicating if the user is logged in
   def logged_in?
     !!current_user
   end
 
-  # Redirects to login page if user is not logged in
+  # Alias the logged_in? method as user_signed_in? for compatibility
+  alias user_signed_in? logged_in?
+
   def require_login
     unless logged_in?
       flash[:alert] = "You must be logged in to access this section"
@@ -21,4 +21,5 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
 
